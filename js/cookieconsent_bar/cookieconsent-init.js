7968741eae9174fe10c19ec96d0a72d79bb0d940
ShuMatsuki <shu.matsuki.0625@gmail.com>
2022-06-09 16:13:18 +0900

    xxxx

--- js/cookieconsent_bar/cookieconsent-init.js
+++ js/cookieconsent_bar/cookieconsent-init.js
@@ -60,7 +60,7 @@ cc.run({
     languages: {
         'ja': {
             consent_modal: { // 同意バナーの文言記入箇所
-                description: '電子調達ポータルでクッキー等を使用して、アクセス状況の分析や利便性向上を行っています。同意いただける方は「同意する」をクリックして下さい。 <button type="button" data-cc="c-settings" class="cc-link">個人情報保護について</button>',
+                description: '電子調達ポータルでクッキー等を使用して、アクセス状況の分析や利便性向上を行っています。同意いただける方は「同意する」をクリックして下さい。<button type="button" data-cc="c-settings" class="cc-link">個人情報保護について</button>',
                 secondary_btn: {
                     text: '同意しない',
                     role: 'accept_necessary'        // 'settings' or 'accept_necessary'
